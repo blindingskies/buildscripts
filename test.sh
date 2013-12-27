bb8cfde5268ef3f9c240ed15f4063aa34f47178f
Daniel Thorpe <dan@blindingskies.com>
2013-12-27 17:06:52 +0000

    [master]: Updates test script.
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

--- test.sh
+++ test.sh
@@ -2,32 +2,19 @@
 
 source `dirname $0`/common.cfg      
 
-unlockKeychain
-
 #COMMAND="xcodebuild test -workspace $PROJECT_DIR/$PROJECT_NAME.xcworkspace -sdk iphonesimulator -configuration Debug -scheme \"$TEST_SCHEME\" OTHER_CODE_SIGN_FLAGS=\"--keychain ${KEYCHAIN}\""
 
-COMMAND="xcodebuild test -workspace $PROJECT_DIR/$PROJECT_NAME.xcworkspace -sdk iphonesimulator -configuration Debug -scheme \"$TEST_SCHEME\"
-
-# Delete the .gcda files created by enabling Test Coverage reports in XCode. These can cause "invalid magic number" errors on subsequent builds
-message "Pre-test cleanup"
-GET_BUILD_SETTINGS="$COMMAND -showBuildSettings"
-BUILD_SETTINGS=`eval ${GET_BUILD_SETTINGS}`
-RESULT_COUNT=`echo "$BUILD_SETTINGS" | awk '/CURRENT_ARCH = /{++c} END {print c;}'`
-for i in $(seq 1 $RESULT_COUNT); do
-	CURRENT_ARCH=`echo "$BUILD_SETTINGS" | awk -v i=$i '/CURRENT_ARCH = /{l++; if (l==i) print $3;}'`
-	OBJ_DIR=`echo "$BUILD_SETTINGS" | awk -v i=$i '/OBJECT_FILE_DIR_normal = /{l++; if (l==i) print $3;}'`
-	echo "Deleting *.gcda from $OBJ_DIR/$CURRENT_ARCH"
-	pushdSilent $OBJ_DIR/$CURRENT_ARCH && rm ./*.gcda && popdSilent
-done
+COMMAND="xcodebuild test -workspace "$PROJECT_DIR"/$PROJECT_NAME.xcworkspace -sdk iphonesimulator -configuration Debug -scheme \"$TEST_SCHEME\""
 
 message "Running tests (sdk=iphonesimulator, scheme=$TEST_SCHEME)"
 TEST_OUTPUT="$LOG_DIR/testOutput.log"
-mkdir -p `dirname $TEST_OUTPUT`
+mkdir -p "`dirname "$TEST_OUTPUT"`"
+
 if $VERBOSE; then 
-    COMMAND+=" 2>&1| tee $TEST_OUTPUT"
+    COMMAND+=" 2>&1| tee "$TEST_OUTPUT""
 else
     # cat is used on purpose, otherwise lines are jumbled by TEST SUCCEEDED message
-    COMMAND+=" 2>&1| cat > $TEST_OUTPUT" 
+    COMMAND+=" 2>&1| cat > "$TEST_OUTPUT"" 
 fi
 eval $COMMAND
 
@@ -35,8 +22,8 @@ eval $COMMAND
 set +e
  
 # Print summary
-RESULTS=`cat $TEST_OUTPUT | grep -i "executed" | tail -n 1`
-cat $TEST_OUTPUT | grep " failed \|TEST FAILED"
+RESULTS=`cat "$TEST_OUTPUT" | grep -i "executed" | tail -n 1`
+cat "$TEST_OUTPUT" | grep " failed \|TEST FAILED"
 if didFail; then 
     printWithColour " :)  " $COL_GREEN
 	echo -e "$RESULTS"
