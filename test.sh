e0ca57c464d4b1f7a47a83bd9d757b87d9c1df78
Daniel Thorpe <dan@blindingskies.com>
2013-12-27 19:48:36 +0000

    [master]: Fixes test scripts.
    Signed-off-by: Daniel Thorpe <dan@blindingskies.com>

--- test.sh
+++ test.sh
@@ -4,17 +4,17 @@ source `dirname $0`/common.cfg
 
 #COMMAND="xcodebuild test -workspace $PROJECT_DIR/$PROJECT_NAME.xcworkspace -sdk iphonesimulator -configuration Debug -scheme \"$TEST_SCHEME\" OTHER_CODE_SIGN_FLAGS=\"--keychain ${KEYCHAIN}\""
 
-COMMAND="xcodebuild test -workspace "$PROJECT_DIR"/$PROJECT_NAME.xcworkspace -sdk iphonesimulator -configuration Debug -scheme \"$TEST_SCHEME\" | xcpretty -tc -r junit"
+COMMAND="xcodebuild test -workspace "$PROJECT_DIR"/$PROJECT_NAME.xcworkspace -sdk iphonesimulator -configuration Debug -scheme \"$TEST_SCHEME\" | xcpretty -r junit"
 
 message "Running tests (sdk=iphonesimulator, scheme=$TEST_SCHEME)"
 TEST_OUTPUT="$LOG_DIR/testOutput.log"
 mkdir -p "`dirname "$TEST_OUTPUT"`"
 
 if $VERBOSE; then 
-    COMMAND+=" 2>&1| tee "$TEST_OUTPUT""
+    COMMAND+=" 2>&1| tee $TEST_OUTPUT"
 else
     # cat is used on purpose, otherwise lines are jumbled by TEST SUCCEEDED message
-    COMMAND+=" 2>&1| cat > "$TEST_OUTPUT"" 
+    COMMAND+=" 2>&1| cat > $TEST_OUTPUT" 
 fi
 eval $COMMAND
 
@@ -22,15 +22,15 @@ eval $COMMAND
 set +e
  
 # Print summary
-RESULTS=`cat "$TEST_OUTPUT" | grep -i "executed" | tail -n 1`
+RESULTS=`cat $TEST_OUTPUT | grep -i "executed" | tail -n 1`
 cat "$TEST_OUTPUT" | grep " failed \|TEST FAILED"
 if didFail; then 
-    printWithColour " :(  " $COL_RED
-	echo -e "$RESULTS"
-	exit 1	
-else
     printWithColour " :)  " $COL_GREEN
 	echo -e "$RESULTS"
+else
+    printWithColour " :(  " $COL_RED
+	echo -e "$RESULTS"
+	exit 1
 fi
 
 set -e
